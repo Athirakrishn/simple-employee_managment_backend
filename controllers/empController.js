acc24ebd7dbcfd26daa0e320463d2aab7133a7ed
Athirakrishn <athiraramakrishnan41020@gmail.com>
2026-01-11 17:45:16 +0530

    crud completed

--- controllers/empController.js
+++ controllers/empController.js
@@ -34,3 +34,49 @@ catch(err){
   }
   
 }
+
+//get one employee
+exports.getSingleEmployee = async(req,res)=>{
+  console.log("inside getSingleEmployee");
+  const {id} = req.params
+  try{
+const singleEmployee =await employees.findById({_id:id})
+
+ res.status(200).json(singleEmployee)
+}
+catch(err){
+ res.status(500).json(err)
+  }
+  
+}
+
+//update one employee
+exports.updateEmployee = async(req,res)=>{
+  console.log("inside updateEmployee");
+  const {id} = req.params
+  const{empMail,empName,salary,designation}=req.body
+  try{
+const updateEmp =await employees.findByIdAndUpdate({_id:id},{empMail,empName,salary,designation},{new:true})
+ 
+ res.status(200).json(updateEmp)
+}
+catch(err){
+ res.status(500).json(err)
+  }
+  
+}
+
+//delete one employee
+exports.removeEmployee = async(req,res)=>{
+  console.log("inside removeEmployee ");
+  const {id} = req.params
+  try{
+const removeEmp =await employees.findByIdAndDelete({_id:id})
+
+ res.status(200).json("Deleted successfully")
+}
+catch(err){
+ res.status(500).json(err)
+  }
+  
+}
--- routing/router.js
+++ routing/router.js
@@ -8,7 +8,15 @@ routes.post('/add-employee',empController.addEmployee)
 //get all emp
 routes.get('/all-employee',empController.getAllEmployee)
 //get single emp
+routes.get('/:id/employee',empController.getSingleEmployee)
+
+
 //update emp 
+routes.put('/:id/employee/update',empController.updateEmployee)
+
 //delete emp
+routes.delete('/:id/employee/remove',empController.removeEmployee)
+
+
 
 module.exports=routes
